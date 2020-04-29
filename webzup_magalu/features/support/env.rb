f2f2d58e13e3bf079f3443bf218e55c0c4f9b386
Caio Cardoso <caioforch10@gmail.com>
2020-04-29 11:03:00 -0300

    Update env.rb

--- webzup_magalu/features/support/env.rb
+++ webzup_magalu/features/support/env.rb
@@ -5,6 +5,6 @@ require "selenium-webdriver"
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   Capybara.page.driver.browser.manage.window.maximize
+  Capybara.default_max_wait_time = 10
 end
 
-Capybara.default_max_wait_time = 10
